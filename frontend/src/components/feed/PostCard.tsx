51e491dc77db92707a35bec56cac71deb0180991
Aditi087 <aditijana0@gmail.com>
2022-11-08 19:30:46 +0530

    post card

---
+++ frontend/src/components/feed/PostCard.tsx
@@ -0,0 +1,90 @@
+import axios from 'axios';
+import { AnyAaaaRecord } from 'dns';
+import React, { useState, useEffect } from 'react';
+import { BsHeart } from 'react-icons/bs';
+import { FaRegComment, FaRetweet } from 'react-icons/fa';
+import { FcLike } from 'react-icons/fc';
+import { FiUpload } from 'react-icons/fi';
+
+interface IPostCard {
+  singlePost: {
+    comment: any;
+    content: String;
+    images: any;
+    like: any;
+    name: String;
+    postId: Number;
+    postAt: String;
+    retweet: any;
+    userId: Number;
+    __v?: Number;
+    _id?: String;
+  };
+}
+
+const PostCard: React.FunctionComponent<IPostCard> = ({ singlePost }) => {
+  console.log(singlePost, 'singlePost');
+
+  const [like, setLike] = useState<boolean>(false);
+
+  const likeButton = () => {
+    if (like === false) {
+      setLike(true);
+    } else if (like === true) {
+      setLike(false);
+    }
+  };
+  return (
+    <>
+      <div className="home_posts">
+        <div className="d-flex">
+          <div className="post_user_img">
+            <img
+              src="https://img.freepik.com/premium-vector/female-user-profile-avatar-is-woman-character-screen-saver-with-emotions_505620-617.jpg?w=2000"
+              alt="img"
+              style={{ height: '50px', width: '50px' }}
+            />
+          </div>
+          <div>
+            <div>
+              <b>{singlePost?.name}</b>
+              <span>@{singlePost?.name}678</span>
+            </div>
+            <p>{singlePost?.content}</p>
+          </div>
+        </div>
+        {singlePost?.images?.map((postImg: any, index2: number) => (
+          <div key={index2}>
+            <img src={postImg?.url} alt="img" style={{ width: '30rem' }} />
+          </div>
+        ))}
+
+        <div className="d-flex justify-content-around py-3">
+          <button className="post_footer_button">
+            <div>
+              <FaRegComment />
+            </div>
+          </button>
+          <button className="post_footer_button">
+            <div>
+              <FaRetweet />
+            </div>
+          </button>
+          <button
+            className="post_footer_button like_button"
+            onClick={likeButton}
+          >
+            <div>{like === false ? <BsHeart /> : <FcLike />}</div>
+          </button>
+          <button className="post_footer_button">
+            <div>
+              <FiUpload />
+            </div>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PostCard;
--- frontend/src/components/feed/home/Home.tsx
+++ frontend/src/components/feed/home/Home.tsx
@@ -1,18 +1,25 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import { Card } from 'react-bootstrap';
-import { BsHeart } from 'react-icons/bs';
-import { FaRegComment, FaRetweet } from 'react-icons/fa';
-import { FiUpload } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
 import HomeHeader from '../../header/HomeHeader';
 import TweetPost from '../../post/TweetPost';
+import PostCard from '../PostCard';
 import './home.css';
+interface IsinglePost {
+  comment: any;
+  content: String;
+  images: any;
+  like: any;
+  name: String;
+  postId: Number;
+  postAt: String;
+  retweet: any;
+  userId: Number;
+  __v?: Number;
+  _id?: String;
+}
 
-interface IHomeProps {}
-
-const Home: React.FunctionComponent<IHomeProps> = (props) => {
-  const [homePost, setHomePost] = useState([]);
+const Home: React.FunctionComponent = () => {
+  const [homePost, setHomePost] = useState<IsinglePost[]>([]);
   useEffect(() => {
     const userId = localStorage.getItem('userId');
     axios
@@ -27,7 +34,8 @@ const Home: React.FunctionComponent<IHomeProps> = (props) => {
         alert(err.response.data.message);
       });
   }, []);
-  console.log(homePost);
+
+  console.log(homePost, 'homePost');
 
   return (
     <>
@@ -35,69 +43,9 @@ const Home: React.FunctionComponent<IHomeProps> = (props) => {
       <div className="home_body">
         <TweetPost />
         {homePost &&
-          homePost?.map((e: any, index1) => (
-            <div className="home_posts" key={index1}>
-              <div className="d-flex">
-                <div className="post_user_img">
-                  <img
-                    src="https://img.freepik.com/premium-vector/female-user-profile-avatar-is-woman-character-screen-saver-with-emotions_505620-617.jpg?w=2000"
-                    alt="img"
-                    style={{ height: '50px', width: '50px' }}
-                  />
-                </div>
-                <div>
-                  <div>
-                    <b>{e?.name}</b>
-                    <span>@{e?.name}678</span>
-                  </div>
-                  <p>{e?.content}</p>
-                </div>
-              </div>
-              {e?.images?.map((postImg: any, index2: number) => (
-                <div key={index2}>
-                  <img
-                    src={postImg?.url}
-                    alt="img"
-                    style={{ width: '30rem' }}
-                  />
-                </div>
-              ))}
-
-              <div className="home_post_footer d-flex justify-content-around">
-                <Link to="#">
-                  <div>
-                    <FaRegComment />
-                  </div>
-                </Link>
-                <Link to="#">
-                  <div>
-                    <FaRetweet />
-                  </div>
-                </Link>
-                <Link to="#">
-                  <div>
-                    <BsHeart />
-                  </div>
-                </Link>
-                <Link to="#">
-                  <div>
-                    <FiUpload />
-                  </div>
-                </Link>
-              </div>
-            </div>
+          homePost?.map((e: IsinglePost, index1) => (
+            <PostCard singlePost={e} key={index1} />
           ))}
-
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-        </Card>
       </div>
     </>
   );
